371749a7f08ced4d64371650aa834586e0068d89
makspribora <pribora2006@mail.ru>
2024-03-09 18:04:12 +0600

    startwindow changes

--- fast-mark/src/components/startWindow/startWindow.jsx
+++ fast-mark/src/components/startWindow/startWindow.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { WorkWindowType } from "../../const/windowTypes"
 import ImageUpload from "../imageUpload"
+import { Typography, Icon, Container} from "@mui/material"
 
 
 export default function StartWindow({setImageURL, setWindowType}) {
@@ -13,24 +14,31 @@ export default function StartWindow({setImageURL, setWindowType}) {
 
     return (
         <>
-            <div className="start-window">
-                <div className="strart-window__description">
-                    <p className="start-window__description-text">
-                        Вас приветствует автоматизатор для создания грамот. 
-                        Я умею автоматически заполнять большое количество документов, 
-                        вставляя нужные данные из таблицы в помеченную область.
-                    </p>
-                    <p className="start-window__description-text">
-                       Для пользования мною нужно ...
-                       {/* TODO: сделать инструкцию */}
-                    </p>
-                </div>
-                <label> загрузи изображение </label>
-                <ImageUpload setImageURL={onImageUpload}></ImageUpload>
-                <div className="start-window__warning">
-                    <h2 className="start-window__warning-title">{userWarning}</h2>
-                </div>
-            </div>
+            <Container
+                sx={{
+                    mt: "2rem"
+                }}
+            >
+                    <div>
+                    <Typography
+                        variant="h4"
+                        component="span"
+                        sx={{ flexGrow: 1 }}
+                    >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </Typography>
+                    </div>
+                    
+                    <div>
+                        <Typography component="span" sx={{ flexGrow: 1 }} variant="h5"> upload image </Typography>
+                        <ImageUpload setImageURL={onImageUpload}></ImageUpload>
+                        <div className="start-window__warning">
+                            <h2 className="start-window__warning-title">{userWarning}</h2>
+                        </div>
+                    </div>
+                    
+            </Container>
         </>
     )
 }
