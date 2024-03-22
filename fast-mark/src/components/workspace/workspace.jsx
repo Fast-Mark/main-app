3a966672abeaf6818f6ae6e602f6d1d57df3cb69
makspribora <pribora2006@mail.ru>
2024-03-22 19:06:23 +0600

    height fix

--- fast-mark/src/components/workspace/workspace.jsx
+++ fast-mark/src/components/workspace/workspace.jsx
@@ -250,7 +250,7 @@ export default function Workspace({backgroundURL}) {
                  onContextMenu={(event) => {onRedactorContextMenu(event)}}
                  onMouseMove={onMouseMove}
                     onMouseUp={onMouseUp}
-                    style={{position:"absolute", left:'15%'}}
+                    style={{position:"absolute", left:'15%', height: "100%"}}
                 >                
                 <img src={`${backgroundURL}`} alt='здесь должен был быть ваш макет' id='redactor-image' className='workspace-redactor__background-image'
                     
