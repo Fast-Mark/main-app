66bed34ee447127d189c8713a3c7b8bf27420405
AmrAlt <amralt88@gmail.com>
2024-03-27 16:52:42 +0200

    улучшены рамки при выделении

--- fast-mark/src/components/choosenElementMask/resizePoint.jsx
+++ fast-mark/src/components/choosenElementMask/resizePoint.jsx
@@ -1,11 +1,16 @@
-import { resizePointClassName } from '../../const/classNameConst'
-import './dragPoints.css'
+import React from 'react';
+import { resizePointClassName } from '../../const/classNameConst';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import './resizePoints.css';
 
 export default function ResizePoint() {
 
     return (
         <>
-            <div className={`${resizePointClassName} right-buttom-point` }></div>
+            <div className={`${resizePointClassName} right-buttom-point`}>
+                
+            </div>
+            {/* <DragIndicatorIcon className={`${resizePointClassName} right-buttom-point`}></DragIndicatorIcon> */}
         </>
     )
 }
--- fast-mark/src/components/elementBlock/elementBlock.jsx
+++ fast-mark/src/components/elementBlock/elementBlock.jsx
@@ -1,5 +1,4 @@
 import "./elementBlock.css"
-import BorderBlock from "../choosenElementMask/borderBlock";
 import { useState, useRef  } from "react";
 import ResizePoint from "../choosenElementMask/resizePoint";
 import DropBlock from "../dropBlock/dropBlock";
@@ -65,7 +64,7 @@ export default function ElementBlock({element, zIndexElement, updateElement, onD
                 type={element.type}
                 blockPosition={{position: "fixed", zIndex: "999", ...dropListPositionRef.current}}></DropBlock>: null}
 
-                <div className="box " style={{...element.contentStyles, width:"100%"}}>
+                <div className="box " style={{...element.contentStyles, width:"100%", border:" 4px dashed #303C6c", borderRadius:"3%"}}>
                     {
                         element.type === imageBlockType ? (
                             <img style={{objectFit: "cover", width: "100%", height: "100%"}} src={element.content}/>
@@ -79,7 +78,6 @@ export default function ElementBlock({element, zIndexElement, updateElement, onD
                     }
                 </div> 
 
-                <BorderBlock></BorderBlock>
                 <ResizePoint></ResizePoint>
             </div>
         )} 
